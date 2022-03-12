b5d0480d61b5dcb16c87026631ab1c11cd465b39
fardantif21 <fardan.al.tif421@polban.ac.id>
2022-03-12 21:11:30 +0700

    menambahkan file NarutoAnime.java

---
+++ src/ClassDesignHints/NarutoAnime.java
@@ -0,0 +1,16 @@
+package ClassDesignHints;
+
+/**
+ *
+ * @author Fardan
+ */
+public class NarutoAnime {
+    
+    public static void main(String[] args) {
+        Sasuke s = new Sasuke();
+        s.printDojutsu();
+        Itachi i = new Sasuke();
+        i.printKekkeiGenkai();
+    }
+    
+}
