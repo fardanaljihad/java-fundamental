aca67b04aa0a64cb1c3ff87d8b8e2cbca4e6c766
fardantif21 <fardan.al.tif421@polban.ac.id>
2022-03-12 21:17:46 +0700

    overriding method printDojutsu()

--- src/ClassDesignHints/Sasuke.java
+++ src/ClassDesignHints/Sasuke.java
@@ -1,13 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ClassDesignHints;
 
 /**
  *
  * @author Fardan
  */
-public class Sasuke {
+public class Sasuke extends Itachi {
+    String Dojutsu = "Sharingan";
     
+    @Override
+    void printDojutsu() {
+        super.printDojutsu();
+        System.out.println(this.Dojutsu);
+    }
 }
